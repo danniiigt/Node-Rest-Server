6871d84f77b9626e239fc6f9e1b6abda5d30c629
danniiigt <danielgonzalezt13@gmail.com>
2022-06-10 20:08:13 +0200

    Cors error solucionado

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {dbConnection} = require('./db/config')
 const fileUpload = require('express-fileupload')
+const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT
